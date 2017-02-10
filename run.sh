8027bfd8a306735a9bd2015a57e36908d633cb0b
Ярослав Шаматієнко <yaroslavshamatienko@Bender.local>
2017-02-10 18:39:08 +0200

    groupadd

--- run.sh
+++ run.sh
@@ -2,17 +2,27 @@
 
 echo 'Update & upgrade system...'
 sudo apt-get update & sudo apt-get upgrade -y
-sudo apt-get install curl
+sudo apt-get install curl -y
 
 echo 'Init Docker repo...'
-sudo apt-get install apt-transport-https ca-certificates software-properties-common
-
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ debian-$(lsb_release -cs) main"
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo touch /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo raspbian-jessie main" > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-cache policy docker-engine
 
 echo 'Install Docker...'
 sudo apt-get update
 sudo apt-get -y install docker-engine
+
+echo 'Run Docker service...'
+sudo service docker start
+
+echo 'Setup Docker...'
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
 
 echo 'Prepare env for containers...'
 mkdir /home/pi/atomdata
