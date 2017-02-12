af6540e558d754468fe838ce23e933a551173a75
Ярослав Шаматієнко <yaroslavshamatienko@Bender.local>
2017-02-12 12:28:25 +0200

    jessy 2017 02 12

---
+++ run_after.sh
@@ -0,0 +1,7 @@
+echo 'Prepare env for containers...'
+mkdir /home/pi/atomdata
+
+echo 'Run Atom container...'
+docker run -d -p 4000-4500:4000-4500 --restart=always --privileged -v /dev/bus/usb:/dev/bus/usb -v /home/pi/atomdata:/distr/data princip/atom2:arm
+
+echo 'Done'
--- run_first.sh
+++ run_first.sh
@@ -12,10 +12,5 @@ sudo systemctl unmask docker.service
 sudo systemctl unmask docker.socket
 sudo systemctl start docker.service
 
-echo 'Prepare env for containers...'
-mkdir /home/pi/atomdata
-
-echo 'Run Atom container...'
-docker run -d -p 4000-4500:4000-4500 --restart=always --privileged -v /dev/bus/usb:/dev/bus/usb -v /home/pi/atomdata:/distr/data --name atom princip/atom2:arm
-
-echo 'Done'
+echo 'System reboot...'
+sudo reboot
