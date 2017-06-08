53a4562716e9aee0ae1f4d20fc21ed565146d3cd
Yaroslav Shamatienko <ShamatienkoYaroslav@users.noreply.github.com>
2017-06-08 12:38:47 +0300

    Create run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,12 @@ apt-get update && apt-get upgrade -y
 apt-get install curl -y
 
 echo 'Install Docker...'
-curl -sSL https://get.docker.com | sh
-
-echo 'Unmask and start service...'
-sudo systemctl unmask docker.service
-sudo systemctl unmask docker.socket
-sudo systemctl start docker.service
+apt-get install docker -y
 
 echo 'Prepare env for containers...'
 mkdir /home/pi/atomdata
 
 echo 'Run Atom container...'
-docker run -d -p 4000-4500:4000-4500 --restart=always --privileged -v /dev/bus/usb:/dev/bus/usb -v /home/pi/atomdata:/distr/data --name atom princip/atom2:arm
+sudo docker run -d -p 4000-4005:4000-4005 --log-driver=none --restart=always --privileged -v /dev:/dev -v /home/pi/atomdata:/distr/data --name atom princip/atom2:arm
 
 echo 'Done'
