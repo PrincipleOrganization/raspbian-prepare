7be2bfa1972623269af039190c0151b075d65129
Yaroslav Shamatienko <ShamatienkoYaroslav@users.noreply.github.com>
2017-06-12 16:29:44 +0300

    Create run_after.sh

--- run_after.sh
+++ run_after.sh
@@ -2,6 +2,6 @@ echo 'Prepare env for containers...'
 mkdir /home/pi/atomdata
 
 echo 'Run Atom container...'
-docker run -d -p 4000-4500:4000-4500 --restart=always --privileged -v /dev/bus/usb:/dev/bus/usb -v /home/pi/atomdata:/distr/data princip/atom2:arm
+docker run -d -p 4000-4005:4000-4005 --log-driver=none --restart always --privileged -v /dev:/dev -v /home/pi/atomdata:/distr/data --name atom princip/atom2:arm
 
 echo 'Done'
