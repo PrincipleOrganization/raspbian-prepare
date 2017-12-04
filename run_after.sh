080822cc30727e769525a3dc11fc7d86dd557523
Yaroslav Shamatienko <ShamatienkoYaroslav@users.noreply.github.com>
2017-12-04 12:56:53 +0200

    Update run_after.sh

--- run_after.sh
+++ run_after.sh
@@ -3,5 +3,4 @@ mkdir /home/pi/atomdata
 
 echo 'Run Atom container...'
 docker run -d -p 4000-4005:4000-4005 --log-driver=none --restart always --privileged -v /dev:/dev -v /home/pi/atomdata:/distr/data --name atom princip/atom2:arm
-
 echo 'Done'
